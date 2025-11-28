b91bdba211975dbed7a04d55ea8e8f408fcc1587
Ebrahim Al-Serri <ebrahim.al-serri@georgebrown.ca>
2025-11-28 10:24:28 -0500

    Add Login page

--- frontend/101085527_comp3123_assignment2_reactjs/src/pages/Login.js
+++ frontend/101085527_comp3123_assignment2_reactjs/src/pages/Login.js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{
+            const response = await axios.post("http://localhost:8080/login", {
+                email,
+                password
+            });
+
+            localStorage.setItem("token", response.data.token);
+
+            navigate("/employees");
+        } catch(err) {
+            setError("Invalid credentials. Please try again.");
+        }
+    };
+
+
     return (
-        <div>
-            <h1>Login Page</h1>
-            <p>Here we will build the login form.</p>
+        <div style={{maxWidth:"400px", margin: "50px auto"}}>
+            <h2>Login</h2>
+
+            {error && <p style={{color:"red"}}>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+
+                <div style={{marginBottom: "10px"}}>
+                    <label>Email</label>
+                    <input 
+                        type="email"
+                        className="form-control"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} required
+                    />
+                </div>
+
+                <div style={{marginBottom: "10px"}}>
+                    <label>Password</label>
+                    <input 
+                        type="password"
+                        className="form-control"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} required
+                    />
+                </div>
+
+                <button className="btn btn-primary" type="submit">Login</button>
+
+            </form>
         </div>
     );
 }
