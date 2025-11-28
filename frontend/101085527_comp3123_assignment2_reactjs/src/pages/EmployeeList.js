f399c35f8b4f029489cf511f82c901c63b16ef60
Ebrahim Al-Serri <ebrahim.al-serri@georgebrown.ca>
2025-11-28 16:49:11 -0500

    Add EmployeeList page

--- frontend/101085527_comp3123_assignment2_reactjs/src/pages/EmployeeList.js
+++ frontend/101085527_comp3123_assignment2_reactjs/src/pages/EmployeeList.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, {useEffect, useState, useContext} from "react";
+import axios from "axios";
+import { AuthContext } from "../context/AuthContext";
 
-function EmployeeList(){
+function EmployeeList() {
+
+    const {token} = useContext(AuthContext);
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8081/api/v1/employees", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then((res) => {
+            setEmployees(res.data);
+        })
+        .catch((err) => {
+            console.log("Error fetching employees:", err);
+        });
+    }, [token]);
+
     return(
-        <div>
-            <h1>Employee Page</h1>
-            <p>Here we will show all employee after login.</p>
+        <div style={{padding: "20px"}}>
+            <h2>Employee List</h2>
+
+            <table border="1" width="100%" cellPadding="10">
+                <thead>
+                    <tr>
+                        <th>Emp ID</th>
+                        <th>Full Name</th>
+                        <th>Email</th>
+                        <th>Department</th>
+                        <th>Position</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {employees.map((emp) => (
+                        <tr key={emp._id}>
+                            <td>{emp.employee_id}</td>
+                            <td>{emp.first_name} {emp.last_name}</td>
+                            <td>{emp.email}</td>
+                            <td>{emp.department}</td>
+                            <td>{emp.position}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
+
 export default EmployeeList;
-
-
-
-
-
-
-
-
